257eafb05771dbf6ae12182881d8655a22a3268f
Raph <bocquel@insa-toulouse.fr>
2026-03-16 14:17:33 +0100

    changed configs

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def translate_english(event):
     Translate English text to Pirate speak.
     """
     
-input_text = web.page["english"]
-    
-english = input_text.value
-    
-output_div = web.page["output"]
-    
-output_div.innerText = arrr.translate(english)
+    input_text = web.page["english"]
+        
+    english = input_text.value
+        
+    output_div = web.page["output"]
+        
+    output_div.innerText = arrr.translate(english)
